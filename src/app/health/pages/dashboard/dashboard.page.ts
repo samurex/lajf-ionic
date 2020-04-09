687f0817603954344748c4be0a960268042d46ee
Jan Lindgren <jhlindgren@gmail.com>
2020-04-09 12:34:20 +0200

    minor changes in layout

--- src/app/health/pages/dashboard/dashboard.page.ts
+++ src/app/health/pages/dashboard/dashboard.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '@lajf-app/health/services/user.service';
 import { Observable } from 'rxjs';
 import { User } from '@lajf-app/health/models';
-import { ModalController } from '@ionic/angular';
+import { ModalController, IonRouterOutlet } from '@ionic/angular';
 import { DeclareModalComponent } from './declare-modal/declare-modal.component';
 import { UtilService } from '@lajf-app/core/services';
 import { DeclarationService } from '@lajf-app/health/services';
@@ -24,6 +24,7 @@ export class DashboardPage implements OnInit {
     private modalController: ModalController,
     private util: UtilService,
     private declarationService: DeclarationService,
+    private routerOutlet: IonRouterOutlet
     ) { }
 
   ngOnInit() {
@@ -43,6 +44,8 @@ export class DashboardPage implements OnInit {
   async openDeclarationModal() {
     const modal = await this.modalController.create({
       component: DeclareModalComponent,
+      swipeToClose: true,
+      presentingElement: this.routerOutlet.nativeEl
     });
     await modal.present();
     const { data } = await modal.onWillDismiss();
